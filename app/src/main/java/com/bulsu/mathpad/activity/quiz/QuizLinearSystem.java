3e9ef2228a942755aac41ed42f7a21807a070637
mnl-assasin <mykelneds.03@gmail.com>
2017-03-30 08:42:45 +0800

    Release V1.0 Kulang

---
+++ app/src/main/java/com/bulsu/mathpad/activity/quiz/QuizLinearSystem.java
@@ -0,0 +1,15 @@
+package com.bulsu.mathpad.activity.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bulsu.mathpad.R;
+
+public class QuizLinearSystem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_linear_system);
+    }
+}
