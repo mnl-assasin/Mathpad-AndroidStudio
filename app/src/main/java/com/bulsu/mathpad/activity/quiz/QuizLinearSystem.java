ddd87a764707cf2f730197e7d184138649013c10
mnl-assasin <mykelneds.03@gmail.com>
2017-03-30 08:43:22 +0800

    Release

--- app/src/main/java/com/bulsu/mathpad/activity/quiz/QuizLinearSystem.java
+++ app/src/main/java/com/bulsu/mathpad/activity/quiz/QuizLinearSystem.java
@@ -1,15 +1,204 @@
 package com.bulsu.mathpad.activity.quiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.bulsu.mathpad.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class QuizLinearSystem extends AppCompatActivity {
+
+    @Bind(R.id.rb1a)
+    RadioButton rb1a;
+    @Bind(R.id.rb1b)
+    RadioButton rb1b;
+    @Bind(R.id.rb1c)
+    RadioButton rb1c;
+    @Bind(R.id.rb1d)
+    RadioButton rb1d;
+    @Bind(R.id.rb2a)
+    RadioButton rb2a;
+    @Bind(R.id.rb2b)
+    RadioButton rb2b;
+    @Bind(R.id.rb2c)
+    RadioButton rb2c;
+    @Bind(R.id.rb2d)
+    RadioButton rb2d;
+    @Bind(R.id.rb3a)
+    RadioButton rb3a;
+    @Bind(R.id.rb3b)
+    RadioButton rb3b;
+    @Bind(R.id.rb3c)
+    RadioButton rb3c;
+    @Bind(R.id.rb3d)
+    RadioButton rb3d;
+    @Bind(R.id.rb4a)
+    RadioButton rb4a;
+    @Bind(R.id.rb4b)
+    RadioButton rb4b;
+    @Bind(R.id.rb4c)
+    RadioButton rb4c;
+    @Bind(R.id.rb4d)
+    RadioButton rb4d;
+    @Bind(R.id.rb5a)
+    RadioButton rb5a;
+    @Bind(R.id.rb5b)
+    RadioButton rb5b;
+    @Bind(R.id.rb5c)
+    RadioButton rb5c;
+    @Bind(R.id.rb5d)
+    RadioButton rb5d;
+    @Bind(R.id.rb6a)
+    RadioButton rb6a;
+    @Bind(R.id.rb6b)
+    RadioButton rb6b;
+    @Bind(R.id.rb6c)
+    RadioButton rb6c;
+    @Bind(R.id.rb6d)
+    RadioButton rb6d;
+    @Bind(R.id.rb7a)
+    RadioButton rb7a;
+    @Bind(R.id.rb7b)
+    RadioButton rb7b;
+    @Bind(R.id.rb7c)
+    RadioButton rb7c;
+    @Bind(R.id.rb7d)
+    RadioButton rb7d;
+    @Bind(R.id.rb8a)
+    RadioButton rb8a;
+    @Bind(R.id.rb8b)
+    RadioButton rb8b;
+    @Bind(R.id.rb8c)
+    RadioButton rb8c;
+    @Bind(R.id.rb8d)
+    RadioButton rb8d;
+    @Bind(R.id.rb9a)
+    RadioButton rb9a;
+    @Bind(R.id.rb9b)
+    RadioButton rb9b;
+    @Bind(R.id.rb9c)
+    RadioButton rb9c;
+    @Bind(R.id.rb9d)
+    RadioButton rb9d;
+    @Bind(R.id.rb10a)
+    RadioButton rb10a;
+    @Bind(R.id.rb10b)
+    RadioButton rb10b;
+    @Bind(R.id.rb10c)
+    RadioButton rb10c;
+    @Bind(R.id.rb10d)
+    RadioButton rb10d;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.activity_quiz_determinants1)
+    LinearLayout activityQuizDeterminants1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_linear_system);
+        ButterKnife.bind(this);
+
+        setTitle("Determinants");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @OnClick(R.id.btnSubmit)
+    public void onClick() {
+        int score = 0;
+        if (rb1c.isChecked())
+            score++;
+        else
+            rb1c.setBackgroundColor(Color.RED);
+
+        if (rb2a.isChecked())
+            score++;
+        else
+            rb2a.setBackgroundColor(Color.RED);
+
+        if (rb3b.isChecked())
+            score++;
+        else
+            rb3b.setBackgroundColor(Color.RED);
+
+        if (rb4c.isChecked())
+            score++;
+        else
+            rb4c.setBackgroundColor(Color.RED);
+
+        if (rb5a.isChecked())
+            score++;
+        else
+            rb5a.setBackgroundColor(Color.RED);
+
+        if (rb6a.isChecked())
+            score++;
+        else
+            rb6a.setBackgroundColor(Color.RED);
+
+        if (rb7d.isChecked())
+            score++;
+        else
+            rb7d.setBackgroundColor(Color.RED);
+
+        if (rb8a.isChecked())
+            score++;
+        else
+            rb8a.setBackgroundColor(Color.RED);
+
+        if (rb9c.isChecked())
+            score++;
+        else
+            rb9c.setBackgroundColor(Color.RED);
+
+        if (rb10b.isChecked())
+            score++;
+        else
+            rb10b.setBackgroundColor(Color.RED);
+
+        Log.d("score", "Total Score: " + score);
+        displayScore(score);
+    }
+
+    private void displayScore(int score) {
+
+        View v = LayoutInflater.from(getApplicationContext()).inflate(R.layout.dialog_score, null);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(v);
+
+        TextView tv = (TextView) v.findViewById(R.id.tv_score);
+        tv.setText(String.valueOf(score));
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        }).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/bulsu/mathpad/fragment/QuizzesFragment.java
+++ app/src/main/java/com/bulsu/mathpad/fragment/QuizzesFragment.java
@@ -1,19 +1,34 @@
 package com.bulsu.mathpad.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.bulsu.mathpad.R;
+import com.bulsu.mathpad.activity.quiz.QuizDeterminants1Activity;
+import com.bulsu.mathpad.activity.quiz.QuizLinearSystem;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class QuizzesFragment extends Fragment {
 
+
+    @Bind(R.id.btnLinearEQ)
+    Button btnLinearEQ;
+    @Bind(R.id.btnMatrices)
+    Button btnMatrices;
+    @Bind(R.id.btnDeterminants)
+    Button btnDeterminants;
 
     public QuizzesFragment() {
         // Required empty public constructor
@@ -24,7 +39,28 @@ public class QuizzesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quizzes, container, false);
+        View view = inflater.inflate(R.layout.fragment_quizzes, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @OnClick({R.id.btnLinearEQ, R.id.btnMatrices, R.id.btnDeterminants})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLinearEQ:
+                startActivity(new Intent(getActivity(), QuizLinearSystem.class));
+                break;
+            case R.id.btnMatrices:
+                break;
+            case R.id.btnDeterminants:
+                startActivity(new Intent(getActivity(), QuizDeterminants1Activity.class));
+                break;
+        }
+    }
 }
